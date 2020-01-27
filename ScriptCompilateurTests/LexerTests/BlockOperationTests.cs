de386cca7a60a8b470da87c5ee663a34864af8f7
Sebastian SÖRNSEN <sebastian.sornsen@gmail.com>
2020-01-27 21:34:12 +0100

    monica monica

---
+++ ScriptCompilateurTests/LexerTests/BlockOperationTests.cs
@@ -0,0 +1,106 @@
+﻿using LangScriptCompilateur.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ScriptCompilateurTests.LexerTests
+{
+    class BlockOperationTests
+    {
+        [Test]
+        public void ForBlock()
+        {
+            List<List<Token>> tokens = new List<List<Token>>() {
+                LexerTestBase.GetTokensForScript("for(int i = 0; i > 0; i++)"),
+            };
+
+            List<Signature> signatures = new List<Signature> {
+                Signature.KW_FOR,
+                Signature.LPAREN,
+                Signature.TYPENAME,
+                Signature.IDENTIFIER,
+                Signature.OP_ASSIGN,
+                Signature.I_CONST,
+                Signature.END,
+                Signature.IDENTIFIER,
+                Signature.OP_GREATER_THAN,
+                Signature.I_CONST,
+                Signature.END,
+                Signature.IDENTIFIER,
+                Signature.OP_INCREMENT,
+                Signature.RPAREN
+            };
+
+            foreach (var token in tokens)
+            {
+                var parsedSignatures = token.Select(a => a.Signature).ToList();
+                Assert.AreEqual(signatures, parsedSignatures);
+            }
+
+        }
+
+        [Test]
+        public void WhileBlock()
+        {
+            List<List<Token>> tokens = new List<List<Token>>() {
+                LexerTestBase.GetTokensForScript("while (a == b) {}"),
+                LexerTestBase.GetTokensForScript("while(a==b){}"),
+                LexerTestBase.GetTokensForScript("while(a == b){}"),
+                LexerTestBase.GetTokensForScript("while (a==b) {}"),
+                LexerTestBase.GetTokensForScript("while (aasdf==basdf) {}"),
+                LexerTestBase.GetTokensForScript("while( aasdf == basdf ){}"),
+                LexerTestBase.GetTokensForScript("while(aasdf==basdf){}"),
+            };
+
+            List<Signature> signatures = new List<Signature> {
+                Signature.KW_WHILE,
+                Signature.LPAREN,
+                Signature.IDENTIFIER,
+                Signature.OP_EQUALS,
+                Signature.IDENTIFIER,
+                Signature.RPAREN,
+                Signature.LBRACE,
+                Signature.RBRACE
+            };
+
+            foreach (var token in tokens)
+            {
+                var parsedSignatures = token.Select(a => a.Signature).ToList();
+                Assert.AreEqual(signatures, parsedSignatures);
+            }
+
+        }
+
+        [Test]
+        public void IfEquals()
+        {
+            List<List<Token>> tokens = new List<List<Token>>() {
+                LexerTestBase.GetTokensForScript("if (a == b) {}"),
+                LexerTestBase.GetTokensForScript("if(a==b){}"),
+                LexerTestBase.GetTokensForScript("if(a == b){}"),
+                LexerTestBase.GetTokensForScript("if (a==b) {}"),
+                LexerTestBase.GetTokensForScript("if (aasdf==basdf) {}"),
+                LexerTestBase.GetTokensForScript("if( aasdf == basdf ){}"),
+                LexerTestBase.GetTokensForScript("if(aasdf==basdf){}"),
+            };
+
+            List<Signature> signatures = new List<Signature> {
+                Signature.KW_IF,
+                Signature.LPAREN,
+                Signature.IDENTIFIER,
+                Signature.OP_EQUALS,
+                Signature.IDENTIFIER,
+                Signature.RPAREN,
+                Signature.LBRACE,
+                Signature.RBRACE
+            };
+
+            foreach (var token in tokens)
+            {
+                var parsedSignatures = token.Select(a => a.Signature).ToList();
+                Assert.AreEqual(signatures, parsedSignatures);
+            }
+        }
+    }
+}
---
+++ ScriptCompilateurTests/LexerTests/OperatorParsingTest.cs
@@ -0,0 +1,151 @@
+using LangScriptCompilateur.Models;
+using NUnit.Framework;
+using ScriptCompilateurTests;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Tests
+{
+    public class OperatorParsingTest
+    {
+        [Test]
+        public void Increment()
+        {
+            List<Token> tokens = LexerTestBase.GetTokensForScript("i++");
+
+            List<Signature> signatures = new List<Signature>
+            {
+                Signature.IDENTIFIER,
+                Signature.OP_INCREMENT
+            };
+
+            var parsedSignatures = tokens.Select(a => a.Signature).ToList();
+            Assert.AreEqual(signatures, parsedSignatures);
+        }
+
+        [Test]
+        public void Not()
+        {
+            List<List<Token>> tokens = new List<List<Token>>()
+            {
+                LexerTestBase.GetTokensForScript("!az"),
+                LexerTestBase.GetTokensForScript("! az"),
+            };
+
+            List<Signature> signatures = new List<Signature>
+            {
+                Signature.OP_NOT,
+                Signature.IDENTIFIER
+            };
+
+            foreach (var token in tokens)
+            {
+                var parsedSignatures = token.Select(a => a.Signature).ToList();
+                Assert.AreEqual(signatures, parsedSignatures);
+            }
+        }
+
+        [Test]
+        public void Or()
+        {
+            List<List<Token>> tokens = new List<List<Token>>()
+            {
+                LexerTestBase.GetTokensForScript("az || bz"),
+                LexerTestBase.GetTokensForScript("az|| bz"),
+                LexerTestBase.GetTokensForScript("az ||bz"),
+                LexerTestBase.GetTokensForScript("az||bz")
+            };
+
+            List<Signature> signatures = new List<Signature>
+            {
+                Signature.IDENTIFIER,
+                Signature.OP_OR,
+                Signature.IDENTIFIER
+            };
+
+            foreach (var token in tokens)
+            {
+                var parsedSignatures = token.Select(a => a.Signature).ToList();
+                Assert.AreEqual(signatures, parsedSignatures);
+            }
+        }
+
+        [Test]
+        public void NotEquals()
+        {
+            List<List<Token>> tokens = new List<List<Token>>()
+            {
+                LexerTestBase.GetTokensForScript("az != bz"),
+                LexerTestBase.GetTokensForScript("az!= bz"),
+                LexerTestBase.GetTokensForScript("az !=bz"),
+                LexerTestBase.GetTokensForScript("az!=bz")
+            };
+
+            List<Signature> signatures = new List<Signature>
+            {
+                Signature.IDENTIFIER,
+                Signature.OP_NOTEQUALS,
+                Signature.IDENTIFIER
+            };
+
+            foreach (var token in tokens)
+            {
+                var parsedSignatures = token.Select(a => a.Signature).ToList();
+                Assert.AreEqual(signatures, parsedSignatures);
+            }
+        }
+       
+        [Test]
+        public void Assign()
+        {
+            List<List<Token>> tokens = new List<List<Token>>()
+            {
+                LexerTestBase.GetTokensForScript("az = bz"),
+                LexerTestBase.GetTokensForScript("az= bz"),
+                LexerTestBase.GetTokensForScript("az =bz"),
+                LexerTestBase.GetTokensForScript("az=bz")
+            };
+
+            List<Signature> signatures = new List<Signature>
+            {
+                Signature.IDENTIFIER,
+                Signature.OP_ASSIGN,
+                Signature.IDENTIFIER
+            };
+
+            foreach (var token in tokens)
+            {
+                var parsedSignatures = token.Select(a => a.Signature).ToList();
+                Assert.AreEqual(signatures, parsedSignatures);
+            }
+        }
+
+        [Test]
+        public void Equals()
+        {
+            List<List<Token>> tokens = new List<List<Token>>()
+            {
+                LexerTestBase.GetTokensForScript("az == bz"),
+                LexerTestBase.GetTokensForScript("az== bz"),
+                LexerTestBase.GetTokensForScript("az ==bz"),
+                LexerTestBase.GetTokensForScript("az==bz")
+            };
+
+            List<Signature> signatures = new List<Signature>
+            {
+                Signature.IDENTIFIER,
+                Signature.OP_EQUALS,
+                Signature.IDENTIFIER
+            };
+
+            foreach (var token in tokens)
+            {
+                var parsedSignatures = token.Select(a => a.Signature).ToList();
+                Assert.AreEqual(signatures, parsedSignatures);
+            }
+        }
+
+
+    }
+}
