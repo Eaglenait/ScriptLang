1d5ed9d0e2473689b3801adf3abebc7fb87fc3a6
sebastian sornsen <s.sornsen@groupe-burrus.tech>
2020-01-28 10:25:14 +0100

    modif tests

---
+++ ScriptCompilateurTests/LexerTests/ConstTests.cs
@@ -0,0 +1,33 @@
+using LangScriptCompilateur.Models;
+using static LangScriptCompilateur.Models.Signature;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ScriptCompilateurTests.LexerTests
+{
+    public class ConstTests
+    {
+        [Test]
+        public void Assignation()
+        {
+            List<List<Token>> tokens = new List<List<Token>>() {
+                LexerTestBase.GetTokensForScript("int i = 0;"),
+                LexerTestBase.GetTokensForScript("float f = 1.22f;"),
+                LexerTestBase.GetTokensForScript(@"string s = ""test"";"),
+
+            List<List<Signature>> signatures = new List<List<Signature>> {
+                new List<Signature>(){TYPENAME, IDENTIFIER, OP_ASSIGN, I_CONST, END},
+                new List<Signature>(){TYPENAME, IDENTIFIER, OP_ASSIGN, F_CONST, END},
+                new List<Signature>(){TYPENAME, IDENTIFIER, OP_ASSIGN, STRINGLITTERAL, END}
+            };
+
+            for(int i = 0; i < tokens.Count; i++)
+            {
+                var parsed = tokens[i].Select(a => a.Signature).ToList();
+                Assert.AreEqual(signatures[i], parsed);
+            }
+        }
+    }
+}
