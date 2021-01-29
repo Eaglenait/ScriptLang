e174d31eebdfac0adbd0b55c88d75aa4c44341df
Sebastian SÖRNSEN <sebastian.sornsen@gmail.com>
2021-01-29 18:27:54 +0100

    mesure du temps d'éxecution

--- LangScriptCompilateur/Compilateur.cs
+++ LangScriptCompilateur/Compilateur.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Linq;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace LangScriptCompilateur
 {
@@ -11,12 +12,19 @@ namespace LangScriptCompilateur
 
         public void Compile(string script)
         {
+            Stopwatch sw = new Stopwatch();
+
             //Step 1 : take the string script and parse the tokens.
             //This enables to make a first syntaxic validation to see if the code is even readable or not.
             //We get a list of tokens that will be parsed into 'instructions'
             var lexer = new Lexer(script);
+            sw.Restart();
             lexer.Execute();
+            sw.Stop();
 
+            Console.WriteLine();
+
+            Console.WriteLine($"Lexer finished in: {sw.ElapsedMilliseconds} ms");
             if (!KompilationLogger.Instance.HasFatal())
             {
                 foreach (var token in lexer.Tokens)
@@ -31,15 +39,23 @@ namespace LangScriptCompilateur
                     Console.WriteLine(string.Format("{0} - {1}", log.Item2, log.Item1));
                 }
             }
+            Console.WriteLine();
 
             //Step 2: based on the tokens that we got from the lexer we parse them to extract executable instructions
             //the parsing creates a execution tree. The trees nodes are the instructions. 
             Parser p = new Parser(lexer.Tokens);
+            sw.Restart();
             p.Execute();
+            sw.Stop();
+
+            Console.WriteLine($"Parser finished in: {sw.ElapsedMilliseconds} ms");
 
             if(!KompilationLogger.Instance.HasFatal())
             {
                 Console.WriteLine("No fatal errors");
+                var nodeStrings = p.Tree.IterateAllTree().Select(e => e.NodeType.ToString());
+                foreach (var str in nodeStrings)
+                    Console.WriteLine(str);
             }
             else
             {
