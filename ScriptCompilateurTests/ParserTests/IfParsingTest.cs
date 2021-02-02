2f2ef3e0487260be39b71152f35c485750495aa1
Sebastian SÖRNSEN <sebastian.sornsen@gmail.com>
2021-02-02 18:33:42 +0100

    ajout test de if avec variable pas déclarée

--- ScriptCompilateurTests/ParserTests/IfParsingTest.cs
+++ ScriptCompilateurTests/ParserTests/IfParsingTest.cs
@@ -1,9 +1,8 @@
-﻿
-using LangScriptCompilateur;
+﻿using LangScriptCompilateur;
 using LangScriptCompilateur.Models;
 using LangScriptCompilateur.Models.Enums;
 using NUnit.Framework;
-using System;
+using System.Linq;
 
 namespace ScriptCompilateurTests.ParserTests
 {
@@ -42,6 +41,38 @@ namespace ScriptCompilateurTests.ParserTests
             Assert.IsTrue(ifNode.Childrens[0] is ComparaisonNode);
             Assert.IsTrue(ifNode.Childrens[1].NodeType == OperationType.BLOCK);
             Assert.IsTrue(ifNode.Childrens[2].NodeType == OperationType.BLOCK);
+        }
+
+        [Test]
+        public void IfWithUndeclaredVariable()
+        {
+            Parser p = ParserTestHelper.GetParserInstanceForScript(
+                        "if(a == 1) {}");
+            p.Execute();
+
+            Assert.IsTrue(KompilationLogger.Instance.HasFatal());
+        }
+
+        [Test]
+        public void IfElseWithContent()
+        {
+            Parser p = ParserTestHelper.GetParserInstanceForScript(
+            "if (1 == 1) {" +
+            "   return 0;" +
+            "}" +
+            "else" +
+            "{" +
+            "   return 1;" +
+            "}");
+
+            p.Execute();
+
+            IfNode ifnode = p.Tree.TreeRoot.Childrens[0] as IfNode;
+
+            //main block
+            Assert.IsTrue(ifnode.Childrens[1].HasChildrens);
+            //else block
+            Assert.IsTrue(ifnode.Childrens[2].HasChildrens);
         }
     }
 }
