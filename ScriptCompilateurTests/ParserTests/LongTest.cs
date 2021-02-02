93770f1427721eded426d02f423ccd148da2113b
Sebastian SÖRNSEN <sebastian.sornsen@gmail.com>
2021-02-02 18:33:58 +0100

    creation test long

---
+++ ScriptCompilateurTests/ParserTests/LongTest.cs
@@ -0,0 +1,23 @@
+﻿using LangScriptCompilateur;
+using NUnit.Framework;
+
+namespace ScriptCompilateurTests.ParserTests
+{
+    public class LongTest
+    {
+        [Test]
+        public void FirstLongTest()
+        {
+            Parser p = ParserTestHelper.GetParserInstanceForScript(
+                 $"int a = 0;"
+                + "if(a == 0)" 
+                + "{"
+                + "  return 0;"
+                + "}");
+            p.Execute();
+
+            Assert.IsTrue(KompilationLogger.Instance.HasFatal());
+        }
+
+    }
+}
